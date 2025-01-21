a0d9cd0e7f3f6dfb6d9e5fa70053fca4c45dccc0
Alexis Huynh <huynhalexis1126@gmail.com>
2025-01-20 19:57:52 -0500

    First test gantt chart library

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
- 
+// import Gantt from "frappe-gantt";
+
+window.onload = function() {
+  let tasks = [
+    {
+      id: '1',
+      name: 'Redesign website',
+      start: '2016-12-28',
+      end: '2016-12-31',
+      progress: 20
+    }
+  ]
+  let gantt = new Gantt("#gantt", tasks, {
+    container_height: "500"
+  });
+};
